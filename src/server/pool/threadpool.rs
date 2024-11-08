d2c5102cf130e21a1587fb6005768d6c13541d74
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2024-11-08 17:19:17 -0300

    feat: implementado una estructura de `ThreadPool`

---
+++ src/server/pool/threadpool.rs
@@ -0,0 +1,73 @@
+//! Módulo para un _ThreadPool_.
+
+use std::sync::{
+    mpsc::{channel, Sender},
+    Arc, Mutex,
+};
+
+use crate::protocol::{aliases::results::Result, errors::error::Error};
+use crate::server::pool::{job::JobType, worker::Worker};
+
+/// Un _ThreadPool_ intenta reutilizar los hilos disponibles para realizar
+/// tareas paralelas.
+pub struct ThreadPool {
+    /// La lista de _workers_ a nuestra disposición.
+    workers: Vec<Worker>,
+
+    /// El canal de envío de tareas.
+    sender: Sender<JobType>,
+}
+
+impl ThreadPool {
+    /// Crea una nueva instancia del _ThreadPool_.
+    pub fn new(workers: Vec<Worker>, sender: Sender<JobType>) -> Self {
+        Self { workers, sender }
+    }
+
+    /// Intenta construir una instancia de _ThreadPool_, con la cantidad de hilos indicada.
+    pub fn build(n_threads: usize) -> Result<Self> {
+        if n_threads <= 0 {
+            return Err(Error::ServerError(format!(
+                "{} no es un número de hilos válidos para el ThreadPool.",
+                n_threads
+            )));
+        }
+
+        let (sender, receiver) = channel::<JobType>();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::<Worker>::with_capacity(n_threads);
+        for i in 0..n_threads {
+            workers.push(Worker::build(i, Arc::clone(&receiver))?);
+        }
+
+        Ok(Self::new(workers, sender))
+    }
+
+    /// Ejecuta una tarea en un hilo disponible.
+    pub fn execute<F>(&self, f: F) -> Result<()>
+    where
+        F: FnOnce() -> Result<()> + Send + 'static,
+    {
+        let job = Box::new(f);
+        if let Err(send_err) = self.sender.send(JobType::NewTask(job)) {
+            return Err(Error::ServerError(format!(
+                "Error mandando código a worker:\n\n{}",
+                send_err
+            )));
+        }
+        Ok(())
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in 0..self.workers.len() {
+            self.sender.send(JobType::Exit);
+        }
+
+        for worker in &mut self.workers {
+            drop(worker);
+        }
+    }
+}
