97ed3b808731a246fa08d3918fa1024cea617390
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2024-11-10 16:53:44 -0300

    feat: se puede insertar en las nuevas tablas

--- src/interface/map/panels.rs
+++ src/interface/map/panels.rs
@@ -1,13 +1,19 @@
 //! Módulo para paneles de la interfaz.
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use eframe::egui::{Button, Color32, Context, Frame, Margin, RichText, ScrollArea, SidePanel, Ui};
 
 use crate::{
     client::{cli::Client, protocol_result::ProtocolResult},
-    data::{airports::Airport, flights::Flight},
-    protocol::aliases::{results::Result, types::Long},
+    data::{airports::Airport, flight_states::FlightState, flights::Flight},
+    protocol::{
+        aliases::{
+            results::Result,
+            types::{Int, Long},
+        },
+        errors::error::Error,
+    },
 };
 
 /// Muestra por un panel lateral los detalles del aeropuerto actualmente seleccionado.
@@ -143,20 +149,46 @@ fn show_airport_info(ui: &mut Ui, airport: &Option<Airport>) {
 
 /// Inserta un nuevo vuelo.
 fn insert_flight(
-    mut client: Client,
+    client: Client,
     timestamp: Long,
     cur_airport: &Airport,
     ex_airport: &Airport,
 ) -> Result<()> {
+    let flight_id = cur_airport.id + ex_airport.id + timestamp as usize;
+
+    let incoming_client = Arc::new(Mutex::new(client));
+    // TODO: calcular el tiempo estimado por distancia en vez de asumir que el vuelo es instantáneo.
+    let incoming_query = format!(
+        "INSERT INTO vuelos_entrantes (id, dest, llegada, pos_lat, pos_lon, estado) VALUES ({}, '{}', {}, {}, {}, '{}');",
+        flight_id as Int, ex_airport.ident, timestamp, cur_airport.position.lat(), cur_airport.position.lon(), FlightState::InCourse
+    );
+
+    let departing_client = Arc::clone(&incoming_client);
+    let departing_query = format!(
+        "INSERT INTO vuelos_salientes (id, orig, salida, pos_lat, pos_lon, estado) VALUES ({}, '{}', {}, {}, {}, '{}');",
+        flight_id as Int, cur_airport.ident, timestamp, cur_airport.position.lat(), cur_airport.position.lon(), FlightState::InCourse
+    );
+
+    send_insert_query(incoming_client, incoming_query.as_str())?;
+    send_insert_query(departing_client, departing_query.as_str())?;
+
+    Ok(())
+}
+
+/// Manda una _query_ para insertar un tipo de vuelo.
+fn send_insert_query(client_lock: Arc<Mutex<Client>>, query: &str) -> Result<()> {
+    let mut client = match client_lock.lock() {
+        Ok(cli) => cli,
+        Err(poison_err) => {
+            return Err(Error::ServerError(format!(
+                "Error de lock envenenado tratando de leer un cliente:\n\n{}",
+                poison_err
+            )))
+        }
+    };
+
     let mut tcp_stream = client.connect()?;
-    let protocol_result = client.send_query(
-        format!(
-            "INSERT INTO vuelos (orig, dest, take_off) VALUES ('{}', '{}', {});",
-            cur_airport.ident, ex_airport.ident, timestamp
-        )
-        .as_str(),
-        &mut tcp_stream,
-    )?;
+    let protocol_result = client.send_query(query, &mut tcp_stream)?;
 
     if let ProtocolResult::QueryError(err) = protocol_result {
         println!("{}", err);
