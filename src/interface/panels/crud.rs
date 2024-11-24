ede78a7f40654d9e117dd9a4b9dde18474fbbf58
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2024-11-24 17:53:12 -0300

    fix: La interfaz no debería insertar datos en vivo

--- src/interface/panels/crud.rs
+++ src/interface/panels/crud.rs
@@ -3,14 +3,12 @@
 use std::sync::{Arc, Mutex};
 
 use crate::{
-    client::{cli::Client, protocol_result::ProtocolResult},
+    client::cli::Client,
     data::{airports::airp::Airport, flights::states::FlightState, utils::distances::distance_eta},
-    protocol::{
-        aliases::{
-            results::Result,
-            types::{Int, Long},
-        },
-        errors::error::Error,
+    interface::utils::send_client_query,
+    protocol::aliases::{
+        results::Result,
+        types::{Int, Long},
     },
 };
 
@@ -37,22 +35,8 @@ pub fn insert_flight(
         flight_id as Int, cur_airport.ident, ex_airport.ident, timestamp, FlightState::Preparing
     );
 
-    let inc_tr_cli = Arc::clone(&client);
-    let inc_tr_query = format!(
-        "INSERT INTO vuelos_entrantes_en_vivo (id, orig, dest, llegada, pos_lat, pos_lon, estado, velocidad, altitud, nivel_combustible) VALUES ({}, '{}', '{}', {}, {}, {}, '{}', {}, {}, {});",
-        flight_id as Int, cur_airport.ident, ex_airport.ident, eta, cur_airport.position.lat(), cur_airport.position.lon(), FlightState::Preparing, 0.0, 35000.0, 100.0,
-    );
-
-    let dep_tr_cli = Arc::clone(&client);
-    let dep_tr_query = format!(
-        "INSERT INTO vuelos_salientes_en_vivo (id, orig, dest, llegada, pos_lat, pos_lon, estado, velocidad, altitud, nivel_combustible) VALUES ({}, '{}', '{}', {}, {}, {}, '{}', {}, {}, {});",
-        flight_id as Int, cur_airport.ident, ex_airport.ident, timestamp, cur_airport.position.lat(), cur_airport.position.lon(), FlightState::Preparing, 0.0, 35000.0, 100.0,
-    );
-
     send_client_query(inc_fl_cli, inc_fl_query.as_str())?;
     send_client_query(dep_fl_cli, dep_fl_query.as_str())?;
-    send_client_query(inc_tr_cli, inc_tr_query.as_str())?;
-    send_client_query(dep_tr_cli, dep_tr_query.as_str())?;
 
     Ok(())
 }
@@ -67,28 +51,6 @@ pub fn delete_flight_by_id(client_lock: Arc<Mutex<Client>>, flight_id: Int) -> R
 
     send_client_query(inc_client, inc_delete.as_str())?;
     send_client_query(dep_client, dep_delete.as_str())?;
-
-    Ok(())
-}
-
-/// Manda una _query_ para insertar un tipo de vuelo.
-fn send_client_query(client_lock: Arc<Mutex<Client>>, query: &str) -> Result<()> {
-    let mut client = match client_lock.lock() {
-        Ok(cli) => cli,
-        Err(poison_err) => {
-            return Err(Error::ServerError(format!(
-                "Error de lock envenenado tratando de leer un cliente:\n\n{}",
-                poison_err
-            )))
-        }
-    };
-
-    let mut tcp_stream = client.connect()?;
-    let protocol_result = client.send_query(query, &mut tcp_stream)?;
-
-    if let ProtocolResult::QueryError(err) = protocol_result {
-        println!("{}", err);
-    }
 
     Ok(())
 }
