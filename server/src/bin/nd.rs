12de0e7ee9b4f75b38404d1be65a3fce3f0b1134
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2025-02-19 17:49:15 -0300

    fix: arreglado bug al leer `argv` del sistema
    Ahora, al usar binarios, la cantidad de argumentos es distinta.

--- server/src/bin/nd.rs
+++ server/src/bin/nd.rs
@@ -9,16 +9,18 @@ use {
 fn main() {
     let argv = args().collect::<Vec<String>>();
 
-    if argv.len() >= 3 {
-        if argv[2] == "new" && argv.len() >= 4 {
-            // cargo run nd new <id> <ip> [echo]
-            match argv[3].parse::<Byte>() {
+    if argv.len() >= 2 {
+        if argv[1] == "new" && argv.len() >= 4 {
+            // "target/debug/nd.exe" new <id> <ip> [echo]
+            match argv[2].parse::<Byte>() {
                 Ok(id) => {
-                    if argv[4].parse::<IpAddr>().is_ok() {
-                        println!("Nodo nuevo con id {} y dirección IP {}.", id, argv[4]);
+                    if argv[3].parse::<IpAddr>().is_ok() {
+                        println!("Nodo nuevo con id {} y dirección IP {}.", id, argv[3]);
                         if argv.len() == 5 && argv[4].to_ascii_lowercase() == "echo" {
+                            // "target/debug/nd.exe" new <id> <ip> echo
                             print_err(Node::init_new_in_echo_mode(id, &argv[4]))
                         } else {
+                            // "target/debug/nd.exe" new <id> <ip>
                             print_err(Node::init_new_in_parsing_mode(id, &argv[4]))
                         }
                     } else {
@@ -30,12 +32,14 @@ fn main() {
                 }
             }
         } else {
-            // cargo run nd <id> [echo]
-            match argv[2].parse::<Byte>() {
+            // "target/debug/nd.exe" <id> [echo]
+            match argv[1].parse::<Byte>() {
                 Ok(id) => {
-                    if argv.len() == 4 && argv[3].to_ascii_lowercase() == "echo" {
+                    if argv.len() == 3 && argv[2].to_ascii_lowercase() == "echo" {
+                        // "target/debug/nd.exe" <id> echo
                         print_err(Node::init_in_echo_mode(id))
                     } else {
+                        // "target/debug/nd.exe" <id>
                         print_err(Node::init_in_parsing_mode(id))
                     }
                 }
