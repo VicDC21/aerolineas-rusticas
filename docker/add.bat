03a2536e8d099faca170aaff3984a96f7f97c2c3
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2025-07-27 03:42:22 -0300

    refactor: script mas sofisticado para añadir nodos
    Por ahora sólo en BATCH

--- docker/add.bat
+++ docker/add.bat
@@ -1,7 +1,80 @@
-docker run ^
-       -dit ^
-       --rm ^
-       --network=host ^
-       --name=nodo-%1 ^
-       nodos-slim ^
-       new %1 127.0.0.%1
+@echo off
+
+@REM para poder usar variables con !esto!
+setlocal EnableDelayedExpansion
+
+@REM primero nos aseguramos de que los argumentos existan
+IF "%1"=="" (
+       echo No se especifico el ID del nodo
+       GOTO :EOF
+)
+IF "%2"=="" (
+       echo No se especifico la IP para el nodo de ID %1
+       GOTO :EOF
+)
+
+@REM escribimos el archivo
+(
+echo services:
+echo   nodo_%1:
+echo     container_name: nodo-%1
+echo     depends_on:
+for %%I in (./docker/compose/nodo_*.yaml) do (
+    SET nodo=%%I
+    SET nodo=!nodo:~5,-5!
+    SET /A nodo=nodo
+    IF !nodo! NEQ 0 IF !nodo! LSS %1 (
+echo       - nodo_!nodo!
+    )
+)
+echo     extends:
+echo         file: ./nodo_base.yaml
+echo         service: nodo_base
+echo     networks:
+echo       between_nodes:
+echo         ipv4_address: %2%
+echo     ports:
+echo       - "127.0.0.%1%:6174:6174/tcp"
+echo       - "127.0.0.%1%:8080:8080/tcp"
+echo     command: ["new", "%1", "%2"]
+) > ./docker/compose/nodo_%1.yaml
+
+@REM agregamos también el nodo al compose general
+(
+echo   - ./docker/compose/nodo_%1.yaml
+) >> ./compose.yaml
+
+@REM y al CSV de IPs de cliente
+(
+echo %1,127.0.0.%1
+) >> ./client_ips.csv
+
+@REM y finalmente actualizamos el compose
+docker compose up --detach --no-recreate
+
+@REM antes de que levanten/borren más nodos, sobreescribimos el archivo una vez más para
+@REM hacer pensar que no es nuevo ya
+(
+echo services:
+echo   nodo_%1:
+echo     container_name: nodo-%1
+echo     depends_on:
+for %%I in (./docker/compose/nodo_*.yaml) do (
+    SET nodo=%%I
+    SET nodo=!nodo:~5,-5!
+    SET /A nodo=nodo
+    IF !nodo! NEQ 0 IF !nodo! LSS %1 (
+echo       - nodo_!nodo!
+    )
+)
+echo     extends:
+echo         file: ./nodo_base.yaml
+echo         service: nodo_base
+echo     networks:
+echo       between_nodes:
+echo         ipv4_address: %2%
+echo     ports:
+echo       - "127.0.0.%1%:6174:6174/tcp"
+echo       - "127.0.0.%1%:8080:8080/tcp"
+echo     command: ["%1"]
+) > ./docker/compose/nodo_%1.yaml
