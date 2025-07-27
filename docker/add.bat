81870cb9735c71c93d9a166958f03780ff2a07df
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2025-07-27 18:34:44 -0300

    feat: refinados los scripts para BATCH script de Windows

--- docker/add.bat
+++ docker/add.bat
@@ -2,28 +2,33 @@
 
 @REM para poder usar variables con !esto!
 setlocal EnableDelayedExpansion
+GOTO:cuerpo
 
-@REM primero nos aseguramos de que los argumentos existan
-IF "%1"=="" (
-       echo No se especifico el ID del nodo
-       GOTO :EOF
+@REM Escribe el archivo de compose del nodo
+:compose_nodo
+IF "%~1"=="" (
+    echo No se especifico un ID de nodo.
+    GOTO:EOF
 )
-IF "%2"=="" (
-       echo No se especifico la IP para el nodo de ID %1
-       GOTO :EOF
+IF "%~2"=="" (
+    echo No se especifico la IP para el nodo de ID %~1.
+    GOTO:EOF
+)
+IF "%~3"=="" (
+    echo No se especifico si incluir argumentos de nodo nuevo al nodo de ID %~1.
+    GOTO:EOF
 )
 
-@REM escribimos el archivo
 (
 echo services:
-echo   nodo_%1:
-echo     container_name: nodo-%1
+echo   nodo_%~1:
+echo     container_name: nodo-%~1
 echo     depends_on:
 for %%I in (./docker/compose/nodo_*.yaml) do (
     SET nodo=%%I
     SET nodo=!nodo:~5,-5!
     SET /A nodo=nodo
-    IF !nodo! NEQ 0 IF !nodo! LSS %1 (
+    IF !nodo! NEQ 0 IF !nodo! LSS %~1 (
 echo       - nodo_!nodo!
     )
 )
@@ -32,12 +37,35 @@ echo         file: ./nodo_base.yaml
 echo         service: nodo_base
 echo     networks:
 echo       between_nodes:
-echo         ipv4_address: %2%
+echo         ipv4_address: %~2%
 echo     ports:
-echo       - "127.0.0.%1%:6174:6174/tcp"
-echo       - "127.0.0.%1%:8080:8080/tcp"
-echo     command: ["new", "%1", "%2"]
-) > ./docker/compose/nodo_%1.yaml
+echo       - "127.0.0.%~1:6174:6174/tcp"
+echo       - "127.0.0.%~1:8080:8080/tcp"
+IF %~3==0 (
+echo     command: ["%~1"]
+) ELSE (
+echo     command: ["new", "%~1", "%~2"] 
+)
+) > ./docker/compose/nodo_%~1.yaml
+
+GOTO:EOF
+
+
+@REM el resto del script
+:cuerpo
+
+@REM primero nos aseguramos de que los argumentos existan
+IF "%1"=="" (
+    echo No se especifico el ID del nodo
+    GOTO:EOF
+)
+IF "%2"=="" (
+    echo No se especifico la IP para el nodo de ID %1
+    GOTO:EOF
+)
+
+@REM escribimos el archivo
+CALL:compose_nodo %1 %2 1
 
 @REM agregamos también el nodo al compose general
 (
@@ -50,31 +78,8 @@ echo %1,127.0.0.%1
 ) >> ./client_ips.csv
 
 @REM y finalmente actualizamos el compose
-docker compose up --detach --no-recreate
+@REM docker compose up --detach --no-recreate
 
 @REM antes de que levanten/borren más nodos, sobreescribimos el archivo una vez más para
 @REM hacer pensar que no es nuevo ya
-(
-echo services:
-echo   nodo_%1:
-echo     container_name: nodo-%1
-echo     depends_on:
-for %%I in (./docker/compose/nodo_*.yaml) do (
-    SET nodo=%%I
-    SET nodo=!nodo:~5,-5!
-    SET /A nodo=nodo
-    IF !nodo! NEQ 0 IF !nodo! LSS %1 (
-echo       - nodo_!nodo!
-    )
-)
-echo     extends:
-echo         file: ./nodo_base.yaml
-echo         service: nodo_base
-echo     networks:
-echo       between_nodes:
-echo         ipv4_address: %2%
-echo     ports:
-echo       - "127.0.0.%1%:6174:6174/tcp"
-echo       - "127.0.0.%1%:8080:8080/tcp"
-echo     command: ["%1"]
-) > ./docker/compose/nodo_%1.yaml
+CALL:compose_nodo %1 %2 0
--- docker/remove.bat
+++ docker/remove.bat
@@ -1,8 +1,63 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+GOTO:cuerpo
+
+@REM Lee de un archivo y borra una línea dada.
+@REM Acepta una ruta como 1er arg y un string que es la línea a borrar como 2do arg
+:borrar_linea
+    IF "%~1"=="" (
+        echo No se especifico una ruta. No se hace nada...
+        GOTO:EOF
+    )
+    IF "%~2"=="" (
+        echo No se especifico una linea que eliminar en "%~1". No se hace nada...
+        GOTO:EOF
+    )
+
+    (
+        FOR /F "tokens=* delims=" %%l in (%~1) DO (
+            IF "%%l" NEQ "%~2" (
+                echo %%l
+            )
+        )
+    ) > %~1_temp
+
+    @REM reemplazamos '/' por '\' de ser necesario
+    SET ruta_win=%~1
+    SET ruta_win=%ruta_win:/=\%
+    DEL %ruta_win%
+
+    REN "%ruta_win%_temp" %~nx1
+
+@REM aunque diga EOF, en el contexto de una CALL esto vuelve a donde llamaron a la funcion
+GOTO:EOF
+
+@REM el resto del script, después de las funciones
+:cuerpo
+
+IF "%1"=="" (
+      echo No se incluyo un ID de nodo.
+      GOTO:EOF
+)
+
+@REM borramos el compose del nodo
+@REM necesariamente con backslashes
+DEL ".\docker\compose\nodo_%1.yaml"
+
+@REM borramos la IP del CSV del cliente
+CALL:borrar_linea "./client_ips.csv" "%1,127.0.0.%1"
+
+@REM y lo sacamos del compose general
+CALL:borrar_linea "./compose.yaml" "  - ./docker/compose/nodo_%1.yaml"
+
+@REM y finalmente actualizamos los nodos
 cargo run ^
       --package=server ^
       --bin=nd ^
       delete %1
 
+@REM un poco de tiempo para que se acostumbren los nodos
 timeout /t 10 /nobreak
 
-docker container rm -f nodo-%1
+docker compose up --detach --no-recreate
